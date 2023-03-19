d275922c8ca362d1934c972be0c1b1ecfffdac77
rafieltq <taverasrafiel@gmail.com>
2023-03-19 03:14:00 -0400

    default light theme removin teme provider

--- app/providers.jsx
+++ app/providers.jsx
@@ -1,11 +1,5 @@
 "use client";
 
-import { ThemeProvider } from "next-themes";
-
 export function Providers({ children }) {
-  return (
-    <ThemeProvider attribute="class" enableSystem="false" defaultTheme="light">
-      {children}
-    </ThemeProvider>
-  );
+  return children
 }
